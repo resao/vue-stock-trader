0ef3e6704616566652ed33cc65effaea3b932ee3
Christopher Miles <mail@christophermiles.co.uk>
2018-06-12 19:31:28 +0100

    Add checks to prevent sells if insufficient quantity

--- src/components/portfolio/Stock.vue
+++ src/components/portfolio/Stock.vue
@@ -13,13 +13,15 @@
                         type="number"
                         class="form-control"
                         placeholder="Quantity"
-                        v-model.number="quantity">
+                        v-model.number="quantity"
+                        :class="{danger: insufficientQuantity}">
                 </div>
                 <div class="pull-right">
                     <button
                         class="btn btn-success"
                         @click="sellStock"
-                        :disabled="quantity <= 0 || !Number.isInteger(quantity)">Sell</button>
+                        :disabled="insufficientQuantity || quantity <= 0 || !Number.isInteger(quantity)">
+                        {{ insufficientQuantity ? 'Not enough' : 'Sell' }}</button>
                 </div>
             </div>
         </div>
@@ -34,6 +36,11 @@ export default {
     data() {
         return {
             quantity: 0
+        }
+    },
+    computed: {
+        insufficientQuantity() {
+            return this.quantity > this.stock.quantity;
         }
     },
     methods: {
@@ -53,6 +60,8 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+    .danger {
+        border: 1px solid red;
+    }
 </style>
